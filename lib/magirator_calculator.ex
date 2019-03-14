d1b2eb485c1a263b518acc73f821c0cff71cce20
DevotuOne <devotu.developer@gmail.com>
2019-03-14 23:23:50 +0100

    Winrate + Pdiff

--- lib/magirator_calculator.ex
+++ lib/magirator_calculator.ex
@@ -1,12 +1,32 @@
 defmodule MagiratorCalculator do
 
-  def calculate_points(results) do
-    
-    points =
-      results
+  def calculate_pdiff(results) when length(results) == 0 do
+    0 
+  end
+
+  def calculate_pdiff(results) do
+    wins(results) - losses(results)    
+  end
+
+
+  def calculate_winrate(results) when length(results) == 0 do
+    0.0    
+  end
+
+  def calculate_winrate(results) do
+    wins(results)/Enum.count(results)    
+  end
+
+
+  defp wins(results) do
+    results
       |> Enum.filter( &(&1.place == 1) )
-      |> Enum.map( &(&1).place )
-      |> Enum.sum()
-      
+      |> Enum.count()
+  end
+
+  defp losses(results) do
+    results
+      |> Enum.filter( &(&1.place != 1) )
+      |> Enum.count()
   end
 end
